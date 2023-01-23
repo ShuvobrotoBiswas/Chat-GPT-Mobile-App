e57d9609178b52da37850c5f8c50ecbba838e8aa
SHUVOBROTO BISWAS <93680258+ShuvobrotoBiswas@users.noreply.github.com>
2023-01-23 09:19:56 +0600

    apk exported

---
+++ app/src/main/java/com/example/chatgpt/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.chatgpt;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                super.onReceivedError(view, errorCode, description, failingUrl);
+                // Handle error here
+            }
+        });
+        webView.loadUrl("https://chat.openai.com/chat");
+    }
+}
